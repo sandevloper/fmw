45f94f8bf75fcefa3e9a9f53a9245c5a02facd6d
sandeshBhalekar <sandesh5bhalekar@gmail.com>
2017-10-14 19:39:36 +0530

    up commit

--- www/js/app.js
+++ www/js/app.js
@@ -3,22 +3,130 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+// 'starter.services' is found in services.js
+// 'starter.controllers' is found in controllers.js
+angular.module('starter', ['ionic',
+                          , 'starter.controllers'
+                          , 'starter.services'
+                          , 'starter.directives'
+                          , 'monospaced.elastic'
+                          , 'ksSwiper'
+                          , 'ionic.contrib.ui.tinderCards2'
+              ])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-
-      // Don't remove this line unless you know what you are doing. It stops the viewport
-      // from snapping when text inputs are focused. Ionic handles this internally for
-      // a much nicer keyboard experience.
       cordova.plugins.Keyboard.disableScroll(true);
+
     }
-    if(window.StatusBar) {
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
       StatusBar.styleDefault();
     }
+
+    // Initialize Firebase
+    var config = {
+      apiKey: "AIzaSyBArly16YcseSLD0QeVmgxOKNQPO-N-ONY",
+      authDomain: "find-my-wingman-4c147.firebaseapp.com",
+      databaseURL: "https://find-my-wingman-4c147.firebaseio.com",
+      projectId: "find-my-wingman-4c147",
+      storageBucket: "find-my-wingman-4c147.appspot.com",
+      messagingSenderId: "720284870792"
+    };
+    firebase.initializeApp(config);
+
   });
 })
+.config(function ($provide, $ionicConfigProvider, $compileProvider) {
+  $ionicConfigProvider.tabs.position('bottom');
+  // $ionicConfigProvider.scrolling.jsScrolling(false);
+  // $translateProvider.useStaticFilesLoader({
+  //     prefix: 'l10n/',
+  //     suffix: '.json'
+  //   });
+  // $translateProvider.preferredLanguage("en");
+  // $translateProvider.fallbackLanguage("en");
+  $ionicConfigProvider.scrolling.jsScrolling(false);
+  $compileProvider.imgSrcSanitizationWhitelist(/^\s*(https?|ftp|cdvfile|file|filesystem|blob):|data:image\//);
+  $ionicConfigProvider.backButton.text(null).icon('ion-arrow-left-c color-coral');
+})
+.config(function($stateProvider, $urlRouterProvider) {
+
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+
+  .state('login', {
+    url: '/login',
+    templateUrl: "templates/login.html",
+    controller: 'LoginCtrl'
+  })
+
+  // setup an abstract state for the tabs directive
+    .state('tab', {
+    url: '/tab',
+    abstract: true,
+    templateUrl: 'templates/tabs.html'
+  })
+
+  // Each tab has its own nav history stack:
+
+  .state('tab.dash', {
+    url: '/dash',
+    views: {
+      'tab-dash': {
+        templateUrl: 'templates/tab-dash.html',
+        controller: 'DashCtrl'
+      }
+    }
+  })
+  .state('tab.dash-match', {
+    url: '/dash/:matchId',
+    views: {
+      'tab-dash': {
+        templateUrl: 'templates/tab-dash-match.html',
+        controller: 'MatchCtrl'
+      }
+    }
+  })
+
+  .state('tab.chats', {
+      url: '/chats',
+      views: {
+        'tab-chats': {
+          templateUrl: 'templates/tab-chats.html',
+          controller: 'ChatsCtrl'
+        }
+      }
+    })
+    .state('tab.chat-detail', {
+      url: '/chats/:chatId',
+      views: {
+        'tab-chats': {
+          templateUrl: 'templates/chat-detail.html',
+          controller: 'ChatDetailCtrl'
+        }
+      }
+    })
+
+  .state('tab.account', {
+    url: '/account',
+    views: {
+      'tab-account': {
+        templateUrl: 'templates/tab-account.html',
+        controller: 'AccountCtrl'
+      }
+    }
+  });
+
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/login');
+
+});
